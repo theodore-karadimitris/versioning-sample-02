99e4cc06a7bb8db77b6aa754793c54f5810cbb37
Theodore Karadimitris <theodore.karadimitris@iteam.gr>
2025-07-29 15:22:51 +0300

    feat(api): add /hello endpoint

--- MyDotnetApi/Program.cs
+++ MyDotnetApi/Program.cs
@@ -33,6 +33,8 @@ app.MapGet("/weatherforecast", () =>
 })
 .WithName("GetWeatherForecast");
 
+app.MapGet("/hello", () => "Hello!");
+
 app.Run();
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
